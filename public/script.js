666c6930aa5f534e7468afaa388d357aedff6aa7
fernando-espinosa-clip <fernando.espinosa@payclip.com>
2025-01-28 19:50:01 -0600

    Simplify code and adjust configurations
    Refactored logic in `script.js` to streamline file input handling by consolidating conditional statements. Fixed a minor CSS formatting issue in `styles.css`. Updated `.eslintrc.json` to enable browser environment for ESLint.

--- public/script.js
+++ public/script.js
@@ -4,10 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Detectar cambios en el input de archivo
   fileInput.addEventListener("change", () => {
-    if (fileInput.files.length > 0) {
-      submitBtn.disabled = false; // Habilitar el botón si hay archivo
-    } else {
-      submitBtn.disabled = true; // Deshabilitar el botón si no hay archivo
-    }
+    submitBtn.disabled = fileInput.files.length <= 0;
   });
 });
